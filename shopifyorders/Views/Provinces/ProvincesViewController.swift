625e23f24a07ca0fc4286fb1ee8c7e701adf1dbe
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-07-16 01:40:42 -0400

    Reorganize project, add secondary view controller

---
+++ shopifyorders/Views/Provinces/ProvincesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProvincesViewController.swift
+//  shopifyorders
+//
+//  Created by Mohammed Al-Dahleh on 2018-07-16.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class ProvincesViewController: UIViewController {
+    var provinceOrders: OrderedDictionary<String, Order>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
